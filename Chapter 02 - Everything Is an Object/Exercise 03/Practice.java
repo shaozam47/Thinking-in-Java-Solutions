85c64246d6057729d9314e442f9d55174af36248
shaozam47 <57195585+shaozam47@users.noreply.github.com>
2021-08-11 22:47:20 +0530

    Create Practice.java

---
+++ Chapter 02 - Everything Is an Object/Exercise 03/Practice.java
@@ -0,0 +1,13 @@
+class ATypeName
+{
+    int i;
+    boolean b;
+}
+public class Practice
+{
+    public static void main(String[] args)
+    {
+        ATypeName a = new ATypeName();
+        System.out.println(a.i + " is considered as " + a.b + " in C++");
+    }
+}
