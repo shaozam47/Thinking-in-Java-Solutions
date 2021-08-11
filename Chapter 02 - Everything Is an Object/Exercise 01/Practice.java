037e70f1295cc76991082d1035d3ddba56707a23
shaozam47 <57195585+shaozam47@users.noreply.github.com>
2021-08-11 18:20:53 +0530

    Update Practice.java

--- Chapter 02 - Everything Is an Object/Exercise 01/Practice.java
+++ Chapter 02 - Everything Is an Object/Exercise 01/Practice.java
@@ -1,1 +1,14 @@
-
+class Defaulter
+{
+    int i;
+    char c;
+}
+public class Practice
+{
+    public static void main(String[] args)
+    {
+        Defaulter D = new Defaulter();
+        System.out.println("Integer   = " + D.i);
+        System.out.println("Character = " + D.c);
+    }
+}
